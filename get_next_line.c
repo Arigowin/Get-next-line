d30f331896d75c6005700332e1a10be9a43e2bed
dolewski <dolewski@student.42.fr>
2015-12-13 15:31:50 +0100

    h

--- get_next_line.c
+++ get_next_line.c
@@ -6,53 +6,61 @@
 /*   By: dolewski <dolewski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 15:01:18 by dolewski          #+#    #+#             */
-/*   Updated: 2015/12/11 19:54:15 by dolewski         ###   ########.fr       */
+/*   Updated: 2015/12/12 19:55:56 by dolewski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int		gnl_build(int len, char **tmp, char **line, char **buff)
+{
+	int i;
+
+	i = 0;
+	while (i <= len)
+	{
+		if (((*buff)[i] == '\n' || (*buff)[i] == '\0'))
+		{
+			if (*tmp != NULL)
+				*line = ft_strjoin(*tmp, ft_strsub((*buff), 0, i));
+			else
+				*line = ft_strsub((*buff), 0, i);
+			if ((*tmp = ft_strsub((*buff), i + 1, len - i)) == NULL)
+				return (-1);
+			if (*line != NULL && *tmp != NULL)
+				return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 int		gnl_read(int fd, char **line, char **tmp)
 {
 	char	*buff;
-	int		i;
 	int		len;
+	int		ret;
 
-	i = 0;
 	len = 0;
+	ret = 0;
 	if ((buff = ft_strnew(BUFF_SIZE)) == NULL)
 		return (-1);
 	while ((len = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[len] = '\0';
-
-		while (buff && buff[i] != '\0')
-		{
-			if (buff[i] == '\n')
-			{
-				if (*tmp != NULL)
-					*line = ft_strjoin(*tmp, ft_strsub(buff, 0, i));
-				else
-					*line = ft_strsub(buff, 0, i);
-				*tmp = ft_strsub(buff, i + 1, len - i);
-				if (*line != NULL && *tmp != NULL)
-					return (1);
-			}
-			i++;
-		}
-		i = 0;
-		if (*tmp)
+		ret = gnl_build(len, tmp, line, &buff);
+		if (*tmp && ret != 0)
 		{
 			if ((*tmp = ft_strjoin(*tmp, buff)) == NULL)
 				return (-1);
 		}
-		else
-		{
+		else if (ret != 0)
 			if ((*tmp = ft_strdup(buff)) == NULL)
 				return (-1);
-		}
 	}
-	return (0);
+	if (len == -1)
+		return (-1);
+	return (ret);
 }
 
 int		get_next_line(int const fd, char **line)
