55c68e8ca4385b0a2fa58e7dc1664ada6bbd4ac9
dolewski <dolewski@student.42.fr>
2015-12-15 16:07:19 +0100

    doit fonctionner mtn

--- get_next_line.c
+++ get_next_line.c
@@ -1,69 +1,56 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   gnl.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dolewski <dolewski@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/14 13:34:30 by dolewski          #+#    #+#             */
-/*   Updated: 2015/12/14 13:56:32 by dolewski         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 #include <stdlib.h>
 #include <unistd.h>
 
-static void		gnl_cond_read(char **tmp, char **buff)
-{
-	if (*tmp == NULL)
-		*tmp = ft_strdup(*buff);
-	else
-		*tmp = ft_strjoin(*tmp, *buff);
-}
+#include <stdio.h>
 
-static int		gnl_read(int fd, char **line, t_buff *buff_tmp)
+int		get_next_line(int const fd, char **line)
 {
+	static char		*tmp_buff;
+
 	char			*buff;
-	int				len;
-	size_t			i;
-	char			*tmp;
+	int				len_buff;
 
-	i = 0;
-	tmp = NULL;
-	if ((buff = ft_strnew(BUFF_SIZE)) == NULL)
-		return (-1);
-	while ((len = read(fd, buff, BUFF_SIZE)) > 0)
+	char			*sub;
+	int				i;
+
+	if (ft_strchr(tmp_buff, '\n') != NULL)
 	{
-		buff[len] = '\0';
-		gnl_cond_read(&tmp, &buff);
-	}
-	if (tmp != NULL)
-	{
-		if (((*buff_tmp).buff_t = ft_strsplit(tmp, '\n')) == NULL)
-			return (-1);
-		if ((*line = ft_strdup((*buff_tmp).buff_t[(*buff_tmp).j])) != NULL)
+		i = 0;
+		while (tmp_buff[i] != '\n')
+			i++;
+		*line = ft_strsub(tmp_buff, 0, i);
+		sub = ft_strsub(tmp_buff, i + 1, ft_strlen(tmp_buff));
+		free(tmp_buff);
+		tmp_buff = ft_strdup(sub);
+		if (tmp_buff != NULL)
 			return (1);
 	}
-	if (len == -1)
-		return (-1);
-	return (0);
-}
-
-int				get_next_line(const int fd, char **line)
-{
-	static t_buff		buff_tmp;
-
-	if (buff_tmp.buff_t != NULL)
+	buff = ft_strnew(BUFF_SIZE + 1);
+	while((len_buff = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		buff_tmp.j++;
-		if (buff_tmp.buff_t[buff_tmp.j] == NULL)
-			return (0);
-		*line = ft_strdup(buff_tmp.buff_t[buff_tmp.j]);
-		if (*line != NULL)
-			return (1);
-		else
-			return (-1);
+			if (tmp_buff == NULL)
+				tmp_buff = ft_strdup(buff);
+			else
+				tmp_buff = ft_strjoin(tmp_buff, buff);
 	}
-	return (gnl_read(fd, line, &buff_tmp));
+	if ((ft_strchr(tmp_buff, '\n')))
+	{
+		i = 0;
+		while (tmp_buff[i] != '\n')
+			i++;
+		*line = ft_strsub(tmp_buff, 0, i);
+		sub = ft_strsub(tmp_buff, i + 1, ft_strlen(tmp_buff));
+		free(tmp_buff);
+		tmp_buff = ft_strdup(sub);
+		if (tmp_buff != NULL)
+			return (1);
+
+	}
+	if (len_buff == -1)
+		return (-1);
+	else if (len_buff == 0)
+		return (0);
+	else
+		return (1);
 }
