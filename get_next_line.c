1b7f4cf03832c262c422b2b5c6837c231f6f60be
David OLEWSKI <dolewski@e3r12p15.42.fr>
2015-12-19 14:11:32 +0100

    struct

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dolewski <dolewski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 13:16:05 by dolewski          #+#    #+#             */
-/*   Updated: 2015/12/18 15:44:36 by dolewski         ###   ########.fr       */
+/*   Updated: 2015/12/19 14:10:26 by dolewski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ int		gnl(char **tmp_buff, char **line)
 	char			*sub;
 	int				i;
 
+	printf("fuck%s\n", *tmp_buff);
 	if ((*tmp_buff) != NULL)
 	{
+		printf("fuck\n");
 		i = 0;
 		while ((*tmp_buff)[i] && (*tmp_buff)[i] != '\n')
 			i++;
@@ -84,42 +86,55 @@ int		gnl_read(int fd, char **tmp_buff)
 	return (len_buff);
 }
 
+t_file **ft_search(t_file **lst, int fd)
+{
+	while (*lst != NULL)
+	{
+		if ((*lst)->fd == fd)
+			break;
+		(*lst) = (*lst)->next;
+	}
+	return (lst);
+}
+
 int		get_next_line(int const fd, char **line)
 {
 	//static char		*tmp_buff[1024];
 	static t_file	*tmp_buff;
-	t_file			*tmp;
+	t_file			**tmp;
 	int				ret;
+	char *t;
 
+	t = ft_strnew(1);
+	tmp = &tmp_buff;
+	ft_search(&tmp_buff, fd);
+	t[0] = 'a';
 	if (tmp_buff == NULL)
 	{
-		tmp_buff = ft_lstfilenew(NULL, fd);
-		tmp = tmp_buff;
+		ft_lstfileadd(&tmp_buff, ft_lstfilenew(t, fd));
 	}
-	else
-	{
-		tmp = tmp_buff;
-		while (tmp != NULL && tmp->fd == fd)
-			tmp = tmp->next;
-		if (tmp->fd != fd)
-		{
-			ft_lstfileadd(&tmp_buff, ft_lstfilenew(NULL, fd));
-			tmp = tmp_buff;
-		}
-	}
+	printf("fuck%s\n", tmp_buff->data);
 
 	if (line == NULL)
 		return (-1);
-	if ((ret = gnl(&(tmp->data), line)) == -1)
+	if ((ret = gnl(&(tmp_buff->data), line)) == -1)
 		return (-1);
 	else if (ret == 1)
+	{
+		tmp_buff = *tmp;
 		return (1);
-	if ((gnl_read(tmp->fd, &(tmp->data))) == -1)
+	}
+	if ((gnl_read(fd, &(tmp_buff->data))) == -1)
 		return (-1);
-	if ((ret = gnl(&(tmp->data), line)) == -1)
+	printf("XD%s\n", tmp_buff->data);
+	if ((ret = gnl(&(tmp_buff->data), line)) == -1)
 		return (-1);
 	else if (ret == 1)
+	{
+		tmp_buff = *tmp;
 		return (1);
+	}
 	ft_lstfilefree(&tmp_buff);
+	tmp_buff = *tmp;
 	return (0);
 }
