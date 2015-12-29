44199feff9e683ed94578fe5ef7f358b9305f7c3
dolewski <dolewski@student.42.fr>
2015-12-29 13:21:47 +0100

    main for mult fd

---
+++ main_mfd.c
@@ -0,0 +1,40 @@
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "get_next_line.h"
+
+int main(int ac, char **av)
+{
+	int fd[ac];
+	int ret;
+	char *line;
+	int i;
+
+	i = 0;
+
+	fd[0] = open(av[1], O_RDONLY);
+	fd[1] = open(av[2], O_RDONLY);
+	fd[2] = open(av[3], O_RDONLY);
+	fd[3] = open(av[4], O_RDONLY);
+	ac = 0;
+
+	ret = 1;
+	while (ret > 0)
+	{
+		printf("%d\n", i);
+		ret = get_next_line(fd[i], &line);
+
+		printf("%d->%d->%s\n", ret, fd[i], line);
+		free(line);
+		line = NULL;
+		i = (i == 3 ? 0 : i + 1);
+	}
+
+	close(fd[0]);
+	close(fd[1]);
+	close(fd[2]);
+	close(fd[3]);
+	return (0);
+}
